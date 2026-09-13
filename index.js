8abc5eb397faba86c423c6143ce65baf847916df
Mhn23 <mihneaserban@windowslive.com>
2026-09-14 00:57:01 +0300

    made changes to js, i didnt like the logic, made it more scalable

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-let idValue = 0
+let idValue = 1
 let inputText
+let tasksArray = []
 
-let task = {
-    id: idValue,
-    isCompleted: false,
-    taskName: inputText
-}
-
-let taskArray = []
 const addButton = document.getElementById("add-btn")
 const inputElement = document.getElementById("input-el")
 const tasksUlElement = document.getElementById("tasks-ul-el")
@@ -15,15 +9,14 @@ const tasksUlElement = document.getElementById("tasks-ul-el")
 addButton.addEventListener("click",function(){
     inputText = inputElement.value.trim()
     if(inputText){
-        tasksUlElement.innerHTML += ` 
-                <li class="taskBox" id="task-el-id">
-                    <span>${inputText}</span>
-                    <div class="deleteAndEditButtons">
-                        <button class="editButton">Edit</button>
-                        <button class="deleteButton">Delete</button>
-                    </div>
-                </li>
-                `
+        const task = {
+        id: idValue,
+        isCompleted: false,
+        name: inputText
+        }
+        tasksArray.push(task)
+        localStorage.setItem("tasks",JSON.stringify(tasksArray))
+        
     }else{
         alert("The task can't be an empty space !")
     }
